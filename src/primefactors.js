9138cbf1ef9a80ff45fa98fdb0ddf2d1670eee12
Rija Ménagé <git@rijam.sent.as>
2016-06-23 20:37:56 +0800

    refactored to replace duplicated block with a for loop

--- src/primefactors.js
+++ src/primefactors.js
@@ -6,25 +6,13 @@ module.exports = {
     if (1 < n) {
 
 
-      if (0 === n % 2) {
-        primefactors.push(2)
-        n /= 2
-        if (0 === n % 2) {
-          primefactors.push(2)
-        }
-      }
-      if (0 === n % 3) {
-        primefactors.push(3)
-        n /= 3
-        if (0 === n % 3) {
-          primefactors.push(3)
-        }
-      }
-      if (0 === n % 5) {
-        primefactors.push(5)
-        n /= 5
-        if (0 === n % 5) {
-          primefactors.push(5)
+      for (var i= 2 ; i <= n; i++) {
+        if (0 === n % i) {
+          primefactors.push(i)
+          n /= i
+          if (0 === n % i) {
+            primefactors.push(i)
+          }
         }
       }
 
